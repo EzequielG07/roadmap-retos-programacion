aff346ca46204054e607eb8f9f5736f571cdf94e
EzequielG <ezequiel.guaymas07@gmail.com>
2024-08-28 14:24:29 -0300

    Se empezo el ejercicio de repaso 01

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/0-practica/EzequielG07-01.js
@@ -0,0 +1,51 @@
+//*1
+//Operadores de asignacion
+//asignación
+let x = 5;
+let y = 2;
+console.log(`Asignacion de X=${x} e Y=${y}.`);
+
+//Asignación de adicion
+console.log(`Adicion "x = x + y" --> X=${(x = x + y)}`);
+
+console.log(`Adicion abreviada "x += y" --> X=${(x += y)}`);
+
+// Asignación de resta
+console.log(`Resta "x = x - y" --> X=${(x = x - y)}`);
+
+console.log(`Resta abreviada "x -= y" --> X=${(x -= y)}`);
+
+//Asignación de multiplicación
+console.log(`Multiplicación "x = x * y" --> X=${(x = x * y)}`);
+
+console.log(`Multiplicacion abreviada "x *+ y" --> X=${(x *= y)}`);
+
+//Asignación de división
+console.log(`Division "x = x / y" --> X=${(x = x / y)}`);
+
+console.log(`División abreviada "x /= y" --> X=${(x /= y)}`);
+
+//Asingnación de residuo (Modulo)
+console.log(`Residuo "x = x % y" --> X=${(x = x % y)}`);
+
+console.log(`Residuo abreviado "x %= y --> X=${(x %= y)}`);
+
+//Asignación de exponenciación
+console.log(`Exponenciación "x = x ** y" --> X=${(x = x ** y)}`);
+
+console.log(`Exponenciación abreviada "x **= y" --> X=${(x **= y)}`);
+
+//Asignación de desplazamiento a la izquierda
+console.log(`desplazamiento a la izquierda "x = x << y" --> X=${(x = x << y)}`);
+
+console.log(`desplazamiento a la izquierda abreviado "x <<= y" --> X=${(x <<= y)}`);
+
+//Asignación de desplazamiento a la derecha
+console.log(`desplazamiento a la derecha "x = x >> y" --> X=${(x = x >> y)}`);
+
+console.log(`desplazamiento a la derecha abreviado "x >>= y" --> X=${(x >>= y)}`);
+
+//Asignación de desplazamiento a la derecha sin signo
+console.log(`desplazamiento a la derecha sin signo "x = x >>> y" --> X=${(x = x >>> y)}`);
+
+console.log(`desplazamiento a la derecha sin signo abreviado "x >>>= y" --> X=${(x >>>= y)}`);
